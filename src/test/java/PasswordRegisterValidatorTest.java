74ae4faa434dd4ea311d3690cd861dba03ed3074
Karim <119329701@umail.ucc.ie>
2021-12-02 23:45:02 +0000

    added general layout of PasswordRegisterValidatorTest.java

--- src/test/java/PasswordRegisterValidatorTest.java
+++ src/test/java/PasswordRegisterValidatorTest.java
@@ -1,2 +1,22 @@
+import com.example.a03v2jdk11.EmailRegisterValidator;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PasswordRegisterValidatorTest {
+
+    private PasswordRegisterValidatorTest testedPassword;
+
+    @BeforeEach
+    void starter() {
+        testedPassword = new PasswordRegisterValidator();
+    }
+
+    @Test
+    void checkPassLength(String password) {
+        boolean check = testedPassword.test(password);
+        assertThat(check).isTrue();
+    }
+
+
 }
