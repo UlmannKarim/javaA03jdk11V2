50ebd5be774774c210f609f395dab21c9b058e65
Karim <119329701@umail.ucc.ie>
2021-12-02 21:14:48 +0000

    Updated EmailRegisterValidatorTest.java to test assert if an email is well formatted or not

--- src/test/java/EmailRegisterValidatorTest.java
+++ src/test/java/EmailRegisterValidatorTest.java
@@ -1,2 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class EmailRegisterValidatorTest {
+
+    private EmailRegisterValidator testedEmail;
+
+    @BeforeEach
+    void starter() {
+        testedEmail = new EmailRegisterValidator();
+    }
+
+    @Test
+    void checkEmail(String emailAddress) {
+        boolean check = testedEmail.test(emailAddress);
+        assertThat(check).isTrue();
+    }
 }
