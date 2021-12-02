48f4a01c813441dd79f6edc6284de56f84267922
Karim <119329701@umail.ucc.ie>
2021-12-02 23:15:17 +0000

    improved the regex check to validate the email. Now checks for upper and lower chase characters

--- src/main/java/com/example/a03v2jdk11/EmailRegisterValidator.java
+++ src/main/java/com/example/a03v2jdk11/EmailRegisterValidator.java
@@ -5,7 +5,7 @@ public class EmailRegisterValidator implements Predicate<String> {
 
     @Override
     public boolean test(String emailAddress) {
-        String poss = "^(.+)@(.+)$";
+        String poss = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"; //will check for upper and lower case chars
         return emailAddress.matches(poss); // return true or false
     }
 }
