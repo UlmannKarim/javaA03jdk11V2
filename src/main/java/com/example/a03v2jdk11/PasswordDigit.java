74e0d837b05d245afa9c4554cd84892be38c82ea
Karim <119329701@umail.ucc.ie>
2021-12-03 02:10:57 +0000

    added the test to PasswordDigit.java

--- src/main/java/com/example/a03v2jdk11/PasswordDigit.java
+++ src/main/java/com/example/a03v2jdk11/PasswordDigit.java
@@ -5,6 +5,14 @@ import java.util.function.Predicate;
 public class PasswordDigit implements Predicate<String> {
     @Override
     public boolean test(String Password) {
+        Integer i = 0;
+        while (i <= Password.length()){
+            String stringInt = Integer.toString(i);
+            if (Password.contains(stringInt)) {
+                return true;
+            }
+            i++;
+        }
         return false;
     }
 }
